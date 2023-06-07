1cf0f773890be16f6ddd9bdf2f4bec3240457d9b
Abhishek Turukmane <113658318+abhishekturukmane@users.noreply.github.com>
2023-06-07 16:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius=document.getElementByTagName("input")[0].value;
+	let value=4/3*3.14*radius;
+	return value;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
