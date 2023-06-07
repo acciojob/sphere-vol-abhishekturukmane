56725053bc179fc1bc88ee61c35dcbadd5642f3e
Abhishek Turukmane <113658318+abhishekturukmane@users.noreply.github.com>
2023-06-07 17:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function volume_sphere() {
-    //Write your code here
-	let radius=document.getElementById("radius").value;
-	let value=4/3*3.14*radius;
-	return value;
-} 
+function volume_sphere(event) {
+event.preventDefault();
+let radius = document.getElementById("radius").value;
+let volume = (4 / 3) Math.PI Math.pow(radius, 3);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+document.getElementById("volume").value = volume.toFixed(4); // Adjust precision here
+}
+
+window.onload = function() {
+document.getElementById('MyForm').addEventListener('submit', volume_sphere);
+};
